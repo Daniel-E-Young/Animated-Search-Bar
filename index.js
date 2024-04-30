1d936c9da970a143f502ec758793043417046c7a
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2024-04-30 12:01:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const searchBarContainerEl = document.querySelector(".search-bar-container");
 
 const magnifierEl = document.querySelector(".magnifier");
 
-magnifierEl.addEventListene("click", () => {
+magnifierEl.addEventListener("click", () => {
   searchBarContainerEl.classList.toggle("active");
 });
 
